3dd204f231ec3a2a78726ee387d9869d590b619c
ueta-eisuke <ueta.eisuke1@gmail.com>
2018-08-29 11:55:17 -0700

    fix api and docker yml

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'ac164102fd224eb7b0cca0255be01cd7'
 });
 
 const handleApiCall = (req, res) => {
